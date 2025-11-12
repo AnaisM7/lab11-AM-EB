db2563f3b288eb8867c4311d4e3a3da62bfa6c63
Emy Bijoy <emybijoy06@gmail.com>
2025-11-12 17:19:46 -0500

    Made changes

--- test_calculator.py
+++ test_calculator.py
@@ -13,9 +13,9 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(add(0, 0), 0)
 
     def test_subtract(self): # 3 assertions
-        self.assertEqual(sub(5, 3), 2)
-        self.assertEqual(sub(0, 5), -5)
-        self.assertEqual(sub(-2, -2), 0)
+        self.assertEqual(subtract(5, 3), 2)
+        self.assertEqual(subtract(0, 5), -5)
+        self.assertEqual(subtract(-2, -2), 0)
     ##########################
 
     ####### Partner 1
@@ -36,9 +36,9 @@ class TestCalculator(unittest.TestCase):
             div(0, 5)
 
     def test_logarithm(self): # 3 assertions
-        self.assertAlmostEqual(log(2, 8), 3.0)
-        self.assertAlmostEqual(log(10, 1000), 3.0)
-        self.assertAlmostEqual(log(math.e, math.e ** 2), 2.0)
+        self.assertAlmostEqual(logarithm(2, 8), 3.0)
+        self.assertAlmostEqual(logarithm(10, 1000), 3.0)
+        self.assertAlmostEqual(logarithm(math.e, math.e ** 2), 2.0)
 
     def test_log_invalid_base(self): # 1 assertion
         with self.assertRaises(ValueError):
