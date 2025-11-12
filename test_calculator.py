970ecb7367db5a663bf39efaa5c2896a7d7c91b1
Emy Bijoy <emybijoy06@gmail.com>
2025-11-12 17:22:13 -0500

    Edited log

--- test_calculator.py
+++ test_calculator.py
@@ -42,13 +42,13 @@ class TestCalculator(unittest.TestCase):
 
     def test_log_invalid_base(self): # 1 assertion
         with self.assertRaises(ValueError):
-            log(1, 10)
+            logarithm(1, 10)
     # ##########################
 
     ####### Partner 1
     def test_log_invalid_argument(self): # 1 assertion
         with self.assertRaises(ValueError):
-            log(0, 5)
+            logarithm(10, -5)
 
     def test_hypotenuse(self): # 3 assertions
         self.assertEqual(hypotenuse(3, 4), 5.0)
